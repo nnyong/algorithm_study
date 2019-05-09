c98e0700e4b3fe01f7fc10b6849abe2e2c8e7d6c
nnyong <jo.youthful@gmail.com>
2019-05-09 17:58:56 +0900

    :)

--- 백준/연구소3.py
+++ 백준/연구소3.py
@@ -41,17 +41,19 @@ def virusActive(virus):
             new_y=y+dy[dir]
             new_x=x+dx[dir]
             if isSafe(new_y,new_x):
-                if data[new_y][new_x]==0:
-                    print(new_y,new_x)
-                    if not visited[new_y][new_x]:
+                if not visited[new_y][new_x]:
+                    if data[new_y][new_x]==0:
+                        print(new_y,new_x)
+                        print(data[new_y][new_x])
+                        print(visited[y][x])
                         visited[new_y][new_x]=visited[y][x]+1
-                        que.append((new_y,new_x))
-                elif data[new_y][new_x]==1:
-                    visited[new_y][new_x] = '-'
-                else:
-                    visited[new_y][new_x]='*'
-        if visited[y][x]==0:
-            visited[y][x]='*'
+                        virus.append((new_y,new_x))
+                    elif data[new_y][new_x]==1:
+                        visited[new_y][new_x] = '-'
+                    elif data[new_y][new_x]==2 and (new_y,new_x) not in virus:
+                        visited[new_y][new_x]='*'
+        # if visited[y][x]==0:
+        #     visited[y][x]='*'
         for v in visited:
             print(v)
         print('----------------------')
@@ -59,43 +61,43 @@ def virusActive(virus):
     print(que)
     print('**********************')
 
-T=int(input())
-for tc in range(1,T+1):
-    n,m=map(int,input().split())
-    data=[list(map(int,input().split())) for _ in range(n)]
 
-    # 바이러스 좌표 찾기
-    virus=[]
-    for y in range(n):
-        for x in range(n):
-            if data[y][x]==2:
-                virus.append((y,x))
+n,m=map(int,input().split())
+data=[list(map(int,input().split())) for _ in range(n)]
 
-    # m개의 바이러스 조합 리스트
-    virus_comb=list(map(list,itertools.combinations(virus,m)))
+# 바이러스 좌표 찾기
+virus=[]
+for y in range(n):
+    for x in range(n):
+        if data[y][x]==2:
+            virus.append((y,x))
 
-    # 모든 바이러스 조합에 대해 실행하기!
-    que = []
-    min_time=987654321
-    for v in virus_comb:
-        if complete(data):
-            min_time=0
-            break
-        visited = list([0] * n for _ in range(n))
-        virusActive(v)
-        print('==================')
-        if len(que)!=0:
-            print('que에 값이 있다.que 시작!')
-            virusActive(que)
-            que = []
-            print('조합별 바이러스 확산 시간: {}'.format(Time(visited)))
-        time=Time(visited)
-        if time!=-1:
-            if time<min_time:
-                min_time=time
-        print('바이러스1개 조합 끝')
-    print('================결과===================')
-    if min_time==987654321:
-        print(-1)
-    else:
-        print(min_time)
+# m개의 바이러스 조합 리스트
+virus_comb=list(map(list,itertools.combinations(virus,m)))
+
+# 모든 바이러스 조합에 대해 실행하기!
+que = []
+min_time=987654321
+for v in virus_comb:
+    if complete(data):
+        min_time=0
+        break
+    visited = list([0] * n for _ in range(n))
+    virusActive(v)
+    print('==================')
+    if len(que)!=0:
+        print('que에 값이 있다.que 시작!')
+        virusActive(que)
+        print('que끝!')
+        que = []
+        print('조합별 바이러스 확산 시간: {}'.format(Time(visited)))
+    time=Time(visited)
+    if time!=-1:
+        if time<min_time:
+            min_time=time
+    print('바이러스1개 조합 끝')
+print('================결과===================')
+if min_time==987654321:
+    print(-1)
+else:
+    print(min_time)
